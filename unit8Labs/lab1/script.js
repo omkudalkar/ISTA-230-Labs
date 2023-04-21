fa23bf238c88b8f5f3fec0ab749e8f1ce709cafb
Om Kudalkar <omkudalkar9@gmail.com>
2023-04-20 19:36:20 -0700

    unit8 lab1 done

---
+++ unit8Labs/lab1/script.js
@@ -0,0 +1,9 @@
+function drawTriangle(size) {
+   for (let i = 1; i <= size; i++) {
+     let line = '';
+     for (let j = 1; j <= i; j++) {
+       line += '*';
+     }
+     console.log(line);
+   }
+ }
